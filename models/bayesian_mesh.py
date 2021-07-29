4fbe700501fe086b6c323316cf69e8d3c121475b
Aakanksha Rana <40461936+Aakanksha-Rana@users.noreply.github.com>
2021-07-29 12:52:23 -0400

    changed layer to reparam (for easy warm start and weighnorm_functionality)

--- models/bayesian_mesh.py
+++ models/bayesian_mesh.py
@@ -11,19 +11,18 @@ import tensorflow_probability as tfp
 
 from nobrainer.layers.dropout import BernoulliDropout
 from nobrainer.layers.dropout import ConcreteDropout
-from nobrainer.models.bayesian_utils import normal_prior
 
 tfk = tf.keras
 tfkl = tfk.layers
 tfpl = tfp.layers
-weightnorm = tfp.layers.weight_norm.WeightNorm # This does not work with TFP ConvVariational layers 
 tfd = tfp.distributions
+from bayesian_utils import normal_prior, default_mean_field_normal_fn
+
 def variational_meshnet(
     n_classes,
     input_shape,
     receptive_field=67,
     filters=71,
-    scale_factor = 3000,
     is_monte_carlo=False,
     dropout=None,
     activation=tf.nn.relu,
@@ -31,6 +30,30 @@ def variational_meshnet(
     name="variational_meshnet",
 ):
     """Instantiate variational MeshNet model.
+
+    Please see https://arxiv.org/abs/1805.10863 for more information.
+
+    Parameters
+    ----------
+    n_classes: int, number of classes to classify. For binary applications, use
+        a value of 1.
+    input_shape: list or tuple of four ints, the shape of the input data. Omit
+        the batch dimension, and include the number of channels.
+    receptive_field: {37, 67, 129}, the receptive field of the model. According
+        to the MeshNet manuscript, the receptive field should be similar to your
+        input shape. The actual receptive field is the cube of the value provided.
+    filters: int, number of filters per volumetric convolution. The original
+        MeshNet manuscript uses 21 filters for a binary segmentation task
+        (i.e., brain extraction) and 71 filters for a multi-class segmentation task.
+    activation: str or optimizer object, the non-linearity to use.
+    is_monte_carlo: bool, if true, apply variational approximation in the variational
+        convolution layers. If false, will use the most likely weight (i.e.,
+        the mean of the weight gaussian) instead of sampling a weight from the
+        gaussian distribution.
+    dropout: string, type of dropout layer.
+    batch_size: int, number of samples in each batch. This must be set when
+        training on TPUs.
+    name: str, name to give to the resulting model object.
 
     Returns
     -------
@@ -43,16 +66,22 @@ def variational_meshnet(
 
     if receptive_field not in {37, 67, 129}:
         raise ValueError("unknown receptive field. Legal values are 37, 67, and 129.")
-    
-    def one_layer(x, layer_num, scale_factor = 3000,dilation_rate=(1, 1, 1)):
-        kl_divergence_function = None#(lambda q, p, _: tfd.kl_divergence(q, p) /  # pylint: disable=g-long-lambda
-                            #tf.cast(500, dtype=tf.float32))
-        x = tfpl.Convolution3DFlipout(filters,
-            kernel_size=3, padding="same",dilation_rate=dilation_rate,
-            kernel_prior_fn=prior_fn,
-            #activation=activation, 
-            kernel_divergence_fn=kl_divergence_function, 
-            name="layer{}/vwnconv3d".format(layer_num),)(x)
+    kl_divergence_function = None# (lambda q, p, _: tfd.kl_divergence(q, p) /  # pylint: disable=g-long-lambda
+                           # tf.cast(1600, dtype=tf.float32))
+    def one_layer(x, layer_num, dilation_rate=(1, 1, 1)):
+        # TODO: implement correct behavior for is_mc.
+       
+        x = tfpl.Convolution3DReparameterization(
+            filters,
+            kernel_size=3,
+            padding="same",
+            kernel_prior_fn = normal_prior(),
+            kernel_posterior_fn= default_mean_field_normal_fn(weightnorm= True),
+            kernel_divergence_fn = kl_divergence_function,
+            dilation_rate=dilation_rate,
+            activation=activation,
+            name="layer{}/vwnconv3d".format(layer_num),
+        )(x)
         if dropout is None:
             pass
         elif dropout == "bernoulli":
@@ -71,12 +100,11 @@ def variational_meshnet(
             )(x)
         else:
             raise ValueError("unknown dropout layer, {}".format(dropout))
-        x = tfkl.Activation(activation, name="layer{}/activation".format(layer_num))(x) # This activation makes no sense if Dropout is NONE! 
+        x = tfkl.Activation(activation, name="layer{}/activation".format(layer_num))(x)
         return x
 
     inputs = tfkl.Input(shape=input_shape, batch_size=batch_size, name="inputs")
-    prior_fn = normal_prior(prior_std = 1.0)
-    
+
     if receptive_field == 37:
         x = one_layer(inputs, 1)
         x = one_layer(x, 2)
@@ -102,11 +130,13 @@ def variational_meshnet(
         x = one_layer(x, 6, dilation_rate=(32, 32, 32))
         x = one_layer(x, 7)
 
-    x = tfpl.Convolution3DFlipout(
+    # TODO: implement is_mc behavior.
+    x = tfpl.Convolution3DReparameterization(
         filters=n_classes,
-        kernel_size=1, 
-        kernel_divergence_fn=None,
-	kernel_prior_fn=prior_fn,
+        kernel_size=1,
+        kernel_prior_fn = normal_prior(),
+        kernel_posterior_fn= default_mean_field_normal_fn(weightnorm= True),
+        kernel_divergence_fn = kl_divergence_function,
         padding="same",
         name="classification/vwnconv3d",
     )(x)
