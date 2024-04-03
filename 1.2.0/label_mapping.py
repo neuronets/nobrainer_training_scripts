d4dd275136b42c3c744a6ba12f38bf62358e2fdf
Harsha <hvgazula@users.noreply.github.com>
2024-04-03 06:41:10 -0400

    refactored label mapping

--- 1.2.0/label_mapping.py
+++ 1.2.0/label_mapping.py
@@ -1,51 +1,66 @@
+# Copyright (c) 2024 MIT
+#
+# -*- coding:utf-8 -*-
+# @Script: label_mapping.py
+# @Author: Harsha
+# @Email: hvgazula@users.noreply.github.com
+# @Create At: 2024-04-03 06:37:51
+# @Last Modified By: Harsha
+# @Last Modified At: 2024-04-03 06:40:00
+# @Description:
+# 1. Script to map freesurfer labels to 0-n_classes-1 (segmentation labels).
+# 2. Adapted from the label_mapping.py script written by previous authors.
+
+
 import os
 from typing import Dict
+
 import numpy as np
 import pandas as pd
 
+PRJCT_DIR = "/om2/user/hgazula/nobrainer_training_scripts/"
 LABEL_FILES = {
+    6: "6-class-mapping.csv",
     50: "50-class-mapping.csv",
     115: "115-class-mapping.csv",
-    6: "6-class-mapping.csv",
 }
 
 
-def label_mapping(n_classes: int) -> Dict[int, int]:
+def get_label_mapping(n_classes: int) -> Dict[int, int]:
     """
     Load label mapping for n_classes.
 
     Args:
-        n_classes: Number of classes to load label mapping for.
+        n_classes (int): Number of classes to load label mapping for.
 
     Returns:
         label_mapping: Dictionary mapping original labels to new labels.
 
     Raises:
         NotImplementedError: If n_classes is not 50, 115, or 6.
+
+    The function loads a label mapping file based on the number of classes provided.
+    The conversion is from freesurfer labels to 0-n_classes-1.
+    The unknown class (label 0) is optionally removed from the mapping.
+
+    TODO:
+        1. Add option to remove the unknown class from the mapping.
+        2. Add path to label mapping file.
     """
-    label_file = os.path.join(
-        os.path.dirname(__file__), "csv_files", LABEL_FILES[n_classes]
-    )
+    if n_classes not in [6, 50, 115]:
+        raise NotImplementedError
+
+    label_file = os.path.join(PRJCT_DIR, "csv-files", LABEL_FILES[n_classes])
     print(f"Using label mapping file: {label_file}")
+    print(f"Conversion into {n_classes} segmentation classes from freesurfer labels")
 
-    print(
-        f"Conversion into {n_classes} segmentation classes from freesurfer labels to 0-{n_classes-1}"
-    )
-    if n_classes == 50:
-        tmp = pd.read_csv(label_file, header=0, usecols=[1, 2], dtype=np.int32)
-        tmp = tmp.iloc[1:, :]  # removing the unknown class
-    elif n_classes == 115:
-        tmp = pd.read_csv(label_file, header=0, usecols=[0, 1], dtype=np.int32)
-    elif n_classes == 6:
-        tmp = pd.read_csv(label_file, header=0, usecols=[1, 2], dtype=np.int32)
-    else:
-        raise NotImplementedError(f"n_classes must be 50, 115, or 6, not {n_classes}")
-
-    label_mapping = dict(zip(tmp["original"], tmp["new"]))
-    del tmp
+    df = pd.read_csv(label_file, header=0)
+    df = df[["original", "new"]].astype(np.int32)
+    df = df.iloc[1:, :]
+    label_mapping = dict(zip(df["original"], df["new"]))
 
     return label_mapping
 
 
 if __name__ == "__main__":
-    label_mapping(50)
+    label_mapping = get_label_mapping(6)
