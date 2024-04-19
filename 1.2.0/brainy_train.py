b9c18dadffaedc2c9bd2f12ccc4ab9485a9d1bb6
Harsha <hvgazula@users.noreply.github.com>
2024-04-19 19:17:50 -0400

    added support for plotting predictions in callback

--- 1.2.0/brainy_train.py
+++ 1.2.0/brainy_train.py
@@ -6,20 +6,26 @@
 # @Email: hvgazula@users.noreply.github.com
 # @Create At: 2024-03-29 09:08:29
 # @Last Modified By: Harsha
-# @Last Modified At: 2024-04-12 21:55:23
+# @Last Modified At: 2024-04-19 19:15:59
 # @Description:
 #   1. Code to train brainy (unet) on kwyk dataset.
 #   2. binary segmentation is used in this model.
 #   3. updated to support multi-class segmentation
+#   4. added support for plotting predictions
 
 import os
+import random
+import resource
 import sys
+
+import numpy as np
+from icecream import ic
 
 # ruff: noqa: E402
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 from typing import Dict, Optional
 
-import label_mapping
+import nibabel as nib
 import nobrainer
 import tensorflow as tf
 from nobrainer.dataset import Dataset
@@ -27,7 +33,14 @@ from nobrainer.models import unet
 from nobrainer.processing.segmentation import Segmentation
 from nvitop.callbacks.keras import GpuStatsLogger
 
-from utils import get_git_revision_short_hash, main_timer
+import create_tfshards
+import label_mapping
+from utils import (
+    get_color_map,
+    get_git_revision_short_hash,
+    main_timer,
+    plot_tensor_slices,
+)
 
 # tf.data.experimental.enable_debug_mode()
 
@@ -45,12 +58,18 @@ def load_custom_tfrec(
         data_pattern = (
             "/om2/user/hgazula/nobrainer_training_scripts/1.2.0/data/binseg/*train*"
         )
-    else:
+        # data_pattern = (
+        #     "/om2/user/hgazula/nobrainer_training_scripts/1.2.0/kwyk_10/*train-000*"
+        # )
+    elif target == "eval":
         # data_pattern = "/nese/mit/group/sig/data/kwyk/tfrecords/*eval*"
-        data_pattern = "/om2/user/hgazula/kwyk_records/kwyk_eighth/*eval*01*"
+        data_pattern = "/om2/user/hgazula/kwyk_records/kwyk_quarter/*eval*"
         data_pattern = (
-            "/om2/user/hgazula/nobrainer_training_scripts/1.2.0/data/binseg/*eval*"
+            "/om2/user/hgazula/nobrainer_training_scripts/1.2.0/data/binseg/*eval*000*"
         )
+        # data_pattern = "/om2/user/hgazula/nobrainer_training_scripts/1.2.0/kwyk_25/*eval*"
+    else:
+        data_pattern = "/om2/scratch/Fri/hgazula/kwyk_tfrecords/*test*"
 
     volume_shape = (256, 256, 256)
     block_shape = None
@@ -64,6 +83,81 @@ def load_custom_tfrec(
     )
 
     return dataset
+
+
+class TestCallback(tf.keras.callbacks.Callback):
+    def __init__(self, test_list, cmap, outdir):
+        super(TestCallback, self).__init__()
+        self.test_list = test_list
+        self.outdir = outdir
+        self.cmap = cmap
+        self.slice_dim = np.random.randint(0, 3)
+
+        if not os.path.exists(self.outdir):
+            os.makedirs(self.outdir, exist_ok=True)
+
+    def on_epoch_end(self, epoch, logs=None):
+        print("\nTesting model after epoch {}...".format(epoch + 1))
+
+        # Select a random sample from the test dataset
+        for _ in range(5):
+            random_index = random.randint(0, len(self.test_list) - 1)
+            random_sample = self.test_list[random_index]
+
+            subject_id = os.path.basename(random_sample[0]).split(os.extsep, 1)[0]
+            ic(subject_id)
+
+            pred_outfile_name = os.path.join(
+                self.outdir, f"{subject_id}_epoch-{epoch:02d}_pred.png"
+            )
+            ic(pred_outfile_name)
+            true_outfile_name = os.path.join(
+                self.outdir, f"{subject_id}_epoch-{epoch:02d}_true.png"
+            )
+            ic(true_outfile_name)
+
+            x_data = nib.load(random_sample[0]).get_fdata().astype(np.float32)
+            y_true = nib.load(random_sample[1]).get_fdata().astype(np.float32)
+
+            prediction = self.model.predict(x_data[None, ..., None], batch_size=1)
+            y_pred = np.squeeze(prediction).argmax(-1)
+
+            plot_tensor_slices(
+                y_pred,
+                slice_dim=self.slice_dim,
+                cmap=self.cmap,
+                crop_percentile=10,
+                out_name=pred_outfile_name,
+            )
+            plot_tensor_slices(
+                y_true,
+                slice_dim=self.slice_dim,
+                cmap=self.cmap,
+                crop_percentile=10,
+                out_name=true_outfile_name,
+            )
+
+
+class MemoryLoggerCallback(tf.keras.callbacks.Callback):
+    def __init__(self):
+        self.memory_usage = []
+
+    def on_test_batch_begin(self, batch, logs=None):
+        max_used_memory = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+        print(
+            "Start of batch {}: max memory usage: {} MB".format(batch, max_used_memory)
+        )
+
+    def on_test_batch_end(self, batch, logs=None):
+        max_used_memory = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+        print("End of batch {}: max memory usage: {} MB".format(batch, max_used_memory))
+
+    def on_epoch_end(self, epoch, logs=None):
+        max_used_memory = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+        print(f"\nmemory usage after end of epoch hook: {max_used_memory}")
+        if epoch % 10 == 0:
+            self.memory_usage.append(max_used_memory)
+            print(self.memory_usage)
 
 
 def get_callbacks(
@@ -89,8 +183,11 @@ def get_callbacks(
     )
     callback_gpustats = GpuStatsLogger(gpu_names)
 
+    memory_logger_callback = MemoryLoggerCallback()
+
     callbacks = [
         # callback_gpustats,  # gpu stats callback should be placed before tboard/csvlogger callback
+        # memory_logger_callback,
         callback_model_checkpoint,
         callback_tensorboard,
         callback_early_stopping,
@@ -110,7 +207,7 @@ def main():
     if not NUM_GPUS:
         sys.exit("GPU not found")
 
-    n_epochs = 2
+    n_epochs = 10
     n_classes = 50
     output_dirname = "brainy_mc50"
 
@@ -122,10 +219,27 @@ def main():
         load_custom_tfrec(target="eval", n_classes=n_classes, label_mapping=label_map),
     )
 
-    dataset_train = dataset_train.shuffle(NUM_GPUS).batch(NUM_GPUS)
+    volume_filepaths = create_tfshards.create_filepaths(
+        "/nese/mit/group/sig/data/kwyk/rawdata"
+    )
+    _, _, test_list = create_tfshards.custom_train_val_test_split(
+        volume_filepaths,
+        train_size=0.85,
+        val_size=0.10,
+        test_size=0.05,
+        random_state=42,
+        shuffle=False,
+    )
+
+    test_callback = TestCallback(
+        test_list, get_color_map(n_classes), f"output/{output_dirname}/predictions"
+    )
+
+    dataset_train = dataset_train.repeat(4).shuffle(NUM_GPUS).batch(NUM_GPUS)
     dataset_eval = dataset_eval.batch(NUM_GPUS)
 
     callbacks = get_callbacks(output_dirname=output_dirname, gpu_names=gpu_names)
+    callbacks.append(test_callback)
 
     print("creating model")
     bem = Segmentation(
--- 1.2.0/utils.py
+++ 1.2.0/utils.py
@@ -2,7 +2,20 @@ import csv
 import subprocess
 from datetime import datetime
 
-__all__ = ["main_timer", "_read_csv"]
+import matplotlib.colors as mcolors
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from ext.mindboggle.labels import extract_numbers_names_colors
+
+__all__ = [
+    "main_timer",
+    "_read_csv",
+    "get_git_revision_hash",
+    "get_git_revision_short_hash",
+    "plot_tensor_slices",
+]
 
 
 def main_timer(func):
@@ -44,3 +57,105 @@ def _read_csv(filepath, skip_header=True, delimiter=","):
         if skip_header:
             next(reader)
         return [tuple(row) for row in reader]
+
+
+def crop_tensor(tensor, percentile=10):
+    # Create a copy of the tensor to avoid modifying the original
+    data_for_processing = tensor.copy()
+
+    # Thresholding (assuming background has very low values compared to the head)
+    threshold = np.percentile(data_for_processing, percentile)
+    data_for_processing[data_for_processing < threshold] = 0
+
+    # Find the bounding box around the head (non-zero region) in the filtered data
+    indices = np.nonzero(data_for_processing)
+    min_z, max_z = np.min(indices[0]), np.max(indices[0])
+    min_y, max_y = np.min(indices[1]), np.max(indices[1])
+    min_x, max_x = np.min(indices[2]), np.max(indices[2])
+
+    # Crop the original tensor using the bounding box from the filtered data
+    cropped_tensor = tensor[min_z : max_z + 1, min_y : max_y + 1, min_x : max_x + 1]
+
+    return cropped_tensor
+
+
+def plot_tensor_slices(
+    tensor, slice_dim=0, cmap="viridis", crop_percentile=10, out_name=None
+):
+    # Crop the tensor
+    cropped_tensor = crop_tensor(tensor, percentile=crop_percentile)
+
+    # Determine the dimensions of the cropped tensor
+    dim0, dim1, dim2 = cropped_tensor.shape
+
+    # Determine the slicing dimensions based on the specified slice_dim
+    if slice_dim == 0:
+        num_slices = dim0
+        slice_shape = (dim1, dim2)
+    elif slice_dim == 1:
+        num_slices = dim1
+        slice_shape = (dim0, dim2)
+    elif slice_dim == 2:
+        num_slices = dim2
+        slice_shape = (dim0, dim1)
+    else:
+        raise ValueError("Invalid slice_dim. Must be 0, 1, or 2.")
+
+    # Calculate the grid dimensions
+    grid_size = int(np.ceil(np.sqrt(num_slices)))
+
+    # Create a larger matrix to hold the slices
+    R = np.zeros((grid_size * slice_shape[0], grid_size * slice_shape[1]))
+
+    # Iterate over the slices and place them in the larger matrix
+    for i in range(grid_size):
+        for j in range(grid_size):
+            slice_index = i * grid_size + j
+            if slice_index < num_slices:
+                if slice_dim == 0:
+                    slice_data = cropped_tensor[slice_index, :, :]
+                elif slice_dim == 1:
+                    slice_data = cropped_tensor[:, slice_index, :]
+                else:  # slice_dim == 2
+                    slice_data = cropped_tensor[:, :, slice_index]
+                R[
+                    i * slice_shape[0] : (i + 1) * slice_shape[0],
+                    j * slice_shape[1] : (j + 1) * slice_shape[1],
+                ] = slice_data
+
+    # Plot the larger matrix
+    plt.figure(figsize=(10, 10))
+    plt.imshow(R, cmap=cmap, interpolation="nearest")
+    plt.axis("off")
+    plt.tight_layout()
+    plt.savefig(out_name, dpi=600)
+    plt.show()
+
+
+def get_color_map(n_classes=50):
+    fs_number, fs_names, fs_colors = extract_numbers_names_colors(
+        "/om2/user/hgazula/freesurfer/FreeSurferColorLUT.txt"
+    )
+
+    if n_classes == 115:
+        df = pd.read_csv(
+            "/om2/user/hgazula/nobrainer_training_scripts/csv-files/115-class-mapping.csv",
+            header="infer",
+        )
+    elif n_classes == 50:
+        df = pd.read_csv(
+            "/om2/user/hgazula/nobrainer_training_scripts/csv-files/50-class-mapping.csv",
+            header="infer",
+            index_col=0,
+        )
+    else:
+        raise ValueError("n_classes must be 50 or 115")
+
+    df["colors"] = df["original"].apply(lambda x: fs_colors[fs_number.index(x)])
+    df = df.drop_duplicates(subset="new")
+
+    my_colors = df.colors.tolist()
+    my_colors[0] = [255, 255, 255]  # replacing background with white color
+    cmap = mcolors.ListedColormap(np.array(my_colors) / 255)
+
+    return cmap
