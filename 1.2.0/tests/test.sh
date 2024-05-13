6a684cb7198ab66efae740f088039d697ea80297
H Gazula <hvgazula@users.noreply.github.com>
2024-05-13 07:39:49 -0400

    reorder tests in test.sh

--- 1.2.0/tests/test.sh
+++ 1.2.0/tests/test.sh
@@ -1,16 +1,25 @@
 #!/bin/bash -l
 
-# test training from scratch (binary and multi-class)
-python scripts/train/brainy_train.py tests/configs/brainy_01a.yml
-python scripts/train/brainy_train.py tests/configs/brainy_02a.yml
-
-python scripts/train/kwyk_train.py tests/configs/kwyk_01a.yml
-python scripts/train/kwyk_train.py tests/configs/kwyk_02a.yml
-
-# test resumption (from checkpoint using decorators)
+# test: brainy train and resume (binary)
+rm -rf output/test_brainy_01 && python scripts/train/brainy_train.py tests/configs/brainy_01a.yml
 python scripts/train/brainy_train.py tests/configs/brainy_01b.yml
+
+# test: brainy train and resume (multi-class)
+rm -rf output/test_brainy_02 && python scripts/train/brainy_train.py tests/configs/brainy_02a.yml
+python scripts/train/brainy_train.py tests/configs/brainy_02b.yml
+
+# test: kwyk train and resume (binary)
+rm -rf output/test_kwyk_01 && python scripts/train/kwyk_train.py tests/configs/kwyk_01a.yml
 python scripts/train/kwyk_train.py tests/configs/kwyk_01b.yml
 
-# test decorators
-python scripts/train/decorators.py tests/configs/brainy_01a.yml
-python scripts/train/decorators.py tests/configs/kwyk_01b.yml
+# test: brainy train and resume (multi-class)
+rm -rf output/test_kwyk_02 && python scripts/train/kwyk_train.py tests/configs/kwyk_02a.yml
+python scripts/train/kwyk_train.py tests/configs/kwyk_02b.yml
+
+# test: brainy train and resume (decorators)
+rm -rf output/test_brainy_01 && python scripts/train/decorators.py brainy tests/configs/brainy_01a.yml
+python scripts/train/decorators.py brainy tests/configs/brainy_01b.yml
+
+# test: kwyk train and resume (decorators)
+rm -rf output/test_kwyk_01 && python scripts/train/decorators.py kwyk tests/configs/kwyk_01a.yml
+python scripts/train/decorators.py kwyk tests/configs/kwyk_01b.yml
